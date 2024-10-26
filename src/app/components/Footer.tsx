b1dd48c1fbfbd4a4166d7a0a7725fdca114004fb
Malaika2004 <malaikakhan2204@gmail.com>
2024-10-26 16:30:44 -0700

    Initial commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=' bg-orange-950 py-4 text-center text-amber-700'>&copy;2024 Sugar Canavas All rights reserved.</div>
+  )
+}
+
+export default Footer;
+
+
+ 
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+<div className='h-16   bg-orange-950   text-amber-700 flex justify-between items-center' >
+<h1 className='text-2xl m-4 cursor-pointer'>Sugar Canvas</h1> 
+ 
+  <ul>"Sweteen Your Moments With Sugar Canvas"</ul>
+    </div>
+  )
+}
+export default Navbar;
